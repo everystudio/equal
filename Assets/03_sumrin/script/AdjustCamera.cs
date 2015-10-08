5ebddd7e26f58b33d3d22c28eeeecdf1011ea48f
mainitiamai <mainitiamai@gmail.com>
2015-10-08 14:23:36 +0900

    わからん

---
+++ Assets/03_sumrin/script/AdjustCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustCamera : MonoBehaviour {
+
+	void Awake () {
+		Camera cam = gameObject.GetComponent<Camera>();
+		float baseAspect = 1136.0f/640f;
+		float nowAspect = (float)Screen.height/(float)Screen.width;
+		float changeAspect;
+
+		if(baseAspect>nowAspect){   
+			changeAspect = nowAspect/baseAspect;
+			cam.rect=new Rect((1-changeAspect)*0.5f,0,changeAspect,1);
+		}else{
+			changeAspect = baseAspect/nowAspect;
+			cam.rect=new Rect(0,(1-changeAspect)*0.5f,1,changeAspect);
+		}
+		Destroy(this);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
